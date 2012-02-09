b6e705de6808a337fe913e9e5a4fdc1b6a69e868
kyourek@gmail.com <kyourek@gmail.com@1491abd1-a60c-ce59-6cfc-8684859c51bd>
2012-02-09 04:27:51 +0000

    Added the StyleKeeper.

---
+++ Keeper.OfScripts/StyleKeeper.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Web.Routing;
+
+namespace Keeper.OfScripts
+{
+	public class StyleKeeper
+	{
+		private readonly IStyleGroup _Local;
+		private readonly IStyleGroup _Embedded;
+		private readonly IStyleHelper _Helper;
+		
+		public IStyleGroup Local { get { return _Local; } }
+		public IStyleGroup Embedded { get { return _Embedded; } }
+		public IStyleHelper Helper { get { return _Helper; } }
+		
+		public StyleKeeper(IStyleHelper styleHelper)
+		{
+			if (styleHelper == null) throw new ArgumentNullException("styleHelper");
+			_Helper = styleHelper;
+			_Local = new LocalStyleGroup { Name = "Local", Helper = _Helper.Local };
+			_Embedded = new EmbeddedStyleGroup { Name = "Embedded" };
+		}
+		
+		public string Render()
+		{
+			var str = Embedded.Render();
+			str += Environment.NewLine + Local.Render();
+			return str;
+		}
+	}
+}
