a19cf29e044dfb3fcc35fa0b4652b280a3670c96
kyourek@gmail.com <kyourek@gmail.com@1491abd1-a60c-ce59-6cfc-8684859c51bd>
2012-02-18 21:20:05 +0000

    Tagged version 0.2.2.

---
+++ tags/script-keeper-0.2.2/Keeper.OfScripts.Example/Views/Home/Index.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" Inherits="System.Web.Mvc.ViewPage" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head runat="server"> 
+    <title>ScriptKeeper Example</title>
+    <link rel="stylesheet" type="text/css" href="<%= Url.Content("~/Scripts/fancybox/jquery.fancybox-1.3.4.css") %>" />
+</head>
+<body>
+    <div>
+    <%
+        // We'll render a few pictures that use the fancybox jQuery plugin here.
+        // Notice that none of the scripts required to use fancybox are linked on this page.
+        // They are taken care of in the partial view.
+        var pic1 = new PictureModel { Id = "pic1", Title = "Picture1", Source = "~/Content/Images/dscf1907.jpg", Thumb = "~/Content/Images/dscf1907_thumb.jpg" };
+        var pic2 = new PictureModel { Id = "pic2", Title = "Picture2", Source = "~/Content/Images/dscf1919.jpg", Thumb = "~/Content/Images/dscf1919_thumb.jpg" };
+        var pic3 = new PictureModel { Id = "pic3", Title = "Picture3", Source = "~/Content/Images/dscf1921.jpg", Thumb = "~/Content/Images/dscf1921_thumb.jpg" };
+
+        foreach (var pic in new[] { pic1, pic2, pic3 })
+        {
+            Html.RenderPartial("Picture", pic);
+        }
+    %>
+    
+    </div>
+    
+    <%=
+        // Render the ScriptKeeper's output just before the closing body tag.
+        Html.ScriptKeeper().Render()
+    %>
+</body>
+</html>
---
+++ tags/script-keeper-0.2.2/Keeper.OfScripts.Tests/ScriptGroupTests.cs
@@ -0,0 +1,360 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+using NUnit.Framework;
+
+namespace Keeper.OfScripts.Tests
+{
+	[TestFixture]
+	public class ScriptGroupTests
+	{	
+		[Test]
+		public void ResourceTest()
+		{
+			var file1 = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + 
+				Path.DirectorySeparatorChar + 
+				"Scripts/Script1.js";
+			var file2 = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + 
+				Path.DirectorySeparatorChar + 
+				"Scripts/Script2.js";
+			
+			Assert.IsTrue(File.Exists(file1));
+			Assert.IsTrue(File.Exists(file2));
+		}
+		
+		[Test]
+		public void LocalScriptGroupConstructorTest()
+		{
+			var scriptGroup = new LocalScriptGroup();
+			Assert.IsNotNull(scriptGroup);
+		}
+		
+		[Test]
+		public void InlineScriptGroupConstructorTest()
+		{
+			var scriptGroup = new InlineScriptGroup();	
+			Assert.IsNotNull(scriptGroup);
+		}
+		
+		[Test]
+		public void RemoteScriptGroupConstructorTest()
+		{
+			var scriptGroup = new RemoteScriptGroup();	
+			Assert.IsNotNull(scriptGroup);
+		}
+		
+		[Test]
+		public void LocalScriptGroupAddTest()
+		{
+			var scriptGroup = new LocalScriptGroup();
+			
+			var path = "path/to/script";
+			var script1 = new LinkedScript(path);
+			var script2 = new LinkedScript(path);
+			
+			scriptGroup.Add(script1);
+			
+			try
+			{
+				scriptGroup.Add(script2);
+			}
+			catch (ResourceAlreadyAddedException)
+			{
+				return;
+			}
+			catch (Exception)
+			{
+				Assert.Fail("Incorrect exception thrown.");	
+			}
+			
+			Assert.Fail("No exception thrown.");
+		}
+		
+		[Test]
+		public void RemoteScriptGroupAddTest()
+		{
+			var scriptGroup = new RemoteScriptGroup();
+			
+			var path = "path/to/script";
+			var script1 = new LinkedScript(path);
+			var script2 = new LinkedScript(path);
+			
+			scriptGroup.Add(script1);
+			
+			try
+			{
+				scriptGroup.Add(script2);
+			}
+			catch (ResourceAlreadyAddedException)
+			{
+				return;
+			}
+			catch (Exception)
+			{
+				Assert.Fail("Incorrect exception thrown.");	
+			}
+			
+			Assert.Fail("No exception thrown.");			
+		}
+		
+		[Test]
+		public void InlineScriptGroupAddTest()
+		{
+			var scriptGroup = new InlineScriptGroup();
+			
+			var path = "var s = 'Hello World!';";
+			var script1 = new InlineScript(path);
+			var script2 = new InlineScript(path);
+			
+			scriptGroup.Add(script1);
+			
+			try
+			{
+				scriptGroup.Add(script2);
+			}
+			catch (ResourceAlreadyAddedException)
+			{
+				return;
+			}
+			catch (Exception)
+			{
+				Assert.Fail("Incorrect exception thrown.");	
+			}
+			
+			Assert.Fail("No exception thrown.");			
+		}
+		
+		[Test]
+		public void LocalScriptGroupRegisterTest()
+		{
+			var scriptGroup = new LocalScriptGroup();
+			
+			var path = "path/to/script";
+			var script1 = new LinkedScript(path);
+			var script2 = new LinkedScript(path);
+			
+			scriptGroup.Register(script1);
+			scriptGroup.Register(script2);
+			
+			Assert.AreEqual(1, scriptGroup.Count);
+		}
+		
+		[Test]
+		public void RemoteScriptGroupRegisterTest()
+		{
+			var scriptGroup = new RemoteScriptGroup();
+			
+			var path = "path/to/script";
+			var script1 = new LinkedScript(path);
+			var script2 = new LinkedScript(path);
+			
+			scriptGroup.Register(script1);
+			scriptGroup.Register(script2);
+			
+			Assert.AreEqual(1, scriptGroup.Count);			
+		}
+		
+		[Test]
+		public void InlineScriptGroupRegisterTest()
+		{
+			var scriptGroup = new InlineScriptGroup();
+			
+			var path = "var s = 'Hello World!';";
+			var script1 = new InlineScript(path);
+			var script2 = new InlineScript(path);
+			
+			scriptGroup.Register(script1);
+			scriptGroup.Register(script2);
+			
+			Assert.AreEqual(1, scriptGroup.Count);			
+		}
+		
+		[Test]
+		public void LocalScriptGroupHasRegisteredTest()
+		{
+			var scriptGroup = new LocalScriptGroup();
+			
+			var path = "path/to/script";
+			var script1 = new LinkedScript(path);
+			var script2 = new LinkedScript(path);
+			
+			Assert.IsFalse(scriptGroup.HasRegistered(script1));
+			Assert.IsFalse(scriptGroup.HasRegistered(script2));
+			
+			scriptGroup.Register(script1);
+			
+			Assert.IsTrue(scriptGroup.HasRegistered(script1));
+			Assert.IsTrue(scriptGroup.HasRegistered(script2));
+		}
+		
+		[Test]
+		public void RemoteScriptGroupHasRegisteredTest()
+		{
+			var scriptGroup = new RemoteScriptGroup();
+			
+			var path = "path/to/script";
+			var script1 = new LinkedScript(path);
+			var script2 = new LinkedScript(path);
+			
+			Assert.IsFalse(scriptGroup.HasRegistered(script1));
+			Assert.IsFalse(scriptGroup.HasRegistered(script2));
+			
+			scriptGroup.Register(script1);
+			
+			Assert.IsTrue(scriptGroup.HasRegistered(script1));
+			Assert.IsTrue(scriptGroup.HasRegistered(script2));
+		}
+		
+		[Test]
+		public void InlineScriptGroupHasRegisteredTest()
+		{
+			var scriptGroup = new InlineScriptGroup();
+			
+			var path = "var s = 'Hello World!';";
+			var script1 = new InlineScript(path);
+			var script2 = new InlineScript(path);
+			
+			Assert.IsFalse(scriptGroup.HasRegistered(script1));
+			Assert.IsFalse(scriptGroup.HasRegistered(script2));
+			
+			scriptGroup.Register(script1);
+			
+			Assert.IsTrue(scriptGroup.HasRegistered(script1));
+			Assert.IsTrue(scriptGroup.HasRegistered(script2));
+		}
+		
+		[Test]
+		public void LocalScriptGroupRenderTest()
+		{
+			var scriptGroup = new LocalScriptGroup();
+			
+			var path1 = "path/to/script1";
+			var path2 = "path/to/script2";
+			var script1 = new LinkedScript(path1);
+			var script2 = new LinkedScript(path2);
+			
+			scriptGroup.Add(script1);
+			scriptGroup.Add(script2);
+			
+			Func<string, string> renderTest = s => "<script type=\"text/javascript\" src=\"" + s + "\"></script>";
+
+			var render = scriptGroup.Render();
+			var expected = renderTest(path1) + Environment.NewLine + renderTest(path2);
+
+			Assert.AreEqual(expected, render);
+		}
+		
+		[Test]
+		public void RemoteScriptGroupRenderTest()
+		{
+			var scriptGroup = new RemoteScriptGroup();
+			
+			var path1 = "//path/to/script1";
+			var path2 = "//path/to/script2";
+			var script1 = new LinkedScript(path1);
+			var script2 = new LinkedScript(path2);
+			
+			scriptGroup.Add(script1);
+			scriptGroup.Add(script2);
+			
+			Func<string, string> renderTest = s => "<script type=\"text/javascript\" src=\"" + s + "\"></script>";
+
+			var render = scriptGroup.Render();
+			var expected = renderTest(path1) + Environment.NewLine + renderTest(path2);
+
+			Assert.AreEqual(expected, render);
+		}
+		
+		[Test]
+		public void InlineScriptGroupRenderTest()
+		{
+			var scriptGroup = new InlineScriptGroup();
+			
+			var source1 = "var s = 'Hello World!';";
+			var source2 = "var r = 'Hello Earth!';";
+			var script1 = new InlineScript(source1);
+			var script2 = new InlineScript(source2);
+			
+			scriptGroup.Add(script1);
+			scriptGroup.Add(script2);
+			
+			var expected = "<script type=\"text/javascript\">" + Environment.NewLine;
+			expected += "// <![CDATA[" + Environment.NewLine;
+			expected += source1 + Environment.NewLine;
+			expected += source2 + Environment.NewLine;
+			expected += "// ]]>" + Environment.NewLine;
+			expected += "</script>";
+
+			var render = scriptGroup.Render();
+
+			Assert.AreEqual(expected, render);			
+		}
+		
+		[Test]
+		public void LocalScriptGroupRegisterTest2()
+		{
+			var scriptGroup = new LocalScriptGroup { Helper = new MockLocalHelper() };
+			
+			var script1 = "~/Scripts/Script1.js";
+			var script2 = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "/Scripts/Script1.js";
+			
+			scriptGroup.Register(script1, script2);
+			
+			Assert.AreEqual(1, scriptGroup.Count);
+		}
+		
+		[Test]
+		public void LocalScriptGroupRegisterTest3()
+		{
+			var scriptGroup = new LocalScriptGroup { Helper = new MockLocalHelper() };
+			
+			var script = "~/Scripts/DoesNotExist.js";
+			
+			try
+			{
+				scriptGroup.Register(script);	
+			}
+			catch (ResourceNotFoundException)
+			{
+				return;	
+			}
+			catch (Exception)
+			{
+				Assert.Fail("Wrong exception thrown.");	
+			}
+			
+			Assert.Fail("No exception thrown.");			
+		}
+		
+		[Test]
+		public void RemoteScriptGroupRegisterTest2()
+		{
+			var scriptGroup = new RemoteScriptGroup();
+			
+			var script1 = "http://path/to/script";
+			var script2 = "http://path/to/script";
+			
+			scriptGroup.Register(script1, script2);
+			
+			Assert.AreEqual(1, scriptGroup.Count);
+			Assert.AreEqual(script1, scriptGroup.First().Source);
+		}
+		
+		[Test]
+		public void RemoteScriptRegisterTest3()
+		{
+			var scriptGroup = new RemoteScriptGroup();
+			
+			var script1 = "http://path/to/script1";
+			var script2 = "http://path/to/script2";
+			
+			scriptGroup.Register(script1, script2);
+			
+			Assert.AreEqual(2, scriptGroup.Count);
+			Assert.AreEqual(script1, scriptGroup.First().Source);
+			Assert.AreEqual(script2, scriptGroup.Skip(1).First().Source);
+		}
+	}
+}
