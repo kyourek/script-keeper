284786c0e10a372127371ed12488ee3d137b0e2a
kyourek@gmail.com <kyourek@gmail.com@1491abd1-a60c-ce59-6cfc-8684859c51bd>
2012-02-18 21:06:35 +0000

    Updating NuGet package.

--- Keeper.OfScripts/Properties/AssemblyInfo.cs
+++ Keeper.OfScripts/Properties/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.2.1.0")]
-[assembly: AssemblyFileVersion("0.2.1.0")]
+[assembly: AssemblyVersion("0.2.2")]
+[assembly: AssemblyFileVersion("0.2.2")]
 
 [assembly: InternalsVisibleTo("Keeper.OfScripts.Tests")]
